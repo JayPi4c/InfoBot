b9d803d070332c99027b321425e774488f522f96
JayPi4c <jonas.jp4c@gmail.com>
2020-02-09 14:26:04 +0100

    Reading data from Google Sheets
    With this code it is possible to query on the sheet and get just the
    data one needs to send to the user.
    Further code to listen to Twitter and create charts and so on have to
    be added later.

--- chapter4/index.js
+++ chapter4/index.js
@@ -1,10 +1,32 @@
 require('dotenv').config();
+const https = require('https');
 
-const Twit = require('twit')
+const URL_PRE = `https://docs.google.com/spreadsheets/u/0/d/${process.env.SPREADSHEET_ID}/gviz/tq?tqx=out:csv&pli=1&tq=SELECT+A,+B,+C+WHERE+`;
+const URL_MID = '+%3C+A+and+A+%3C+';
 
-const T = new Twit({
-  consumer_key:         process.env.CONSUMER_KEY,
-  consumer_secret:      process.env.CONSUMER_SECRET,
-  access_token:         process.env.ACCESS_TOKEN,
-  access_token_secret:  process.env.ACCESS_TOKEN_SECRET
-});
+
+function getData(target) {
+
+  let address = URL_PRE + (target - 600) + URL_MID + target;
+
+  https.get(address, (resp) => {
+    let data = '';
+
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    resp.on('end', () => {
+      let result = data.split('\n').splice(1).map(v => v.split(',')).map(v => v.map(v_ => v_.replace(/['"]+/g, '')));
+      // instead of just printing the result we need to create a chart out of it and send it via twitter.
+
+      console.log(result);
+    });
+  }).on('error', (err) => {
+    console.err('ERROR: ' + err.message);
+  });
+
+}
+
+//getData(Math.floor(new Date().getTime()/1000+12*60*60));
+getData(Math.floor(new Date().getTime() / 1000));
