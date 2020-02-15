d1fb0dd1e187660205507a0bff56bfe19962708f
JayPi4c <jonas.jp4c@gmail.com>
2020-02-15 22:47:25 +0100

    Uploading Mastodon bot
    This Bot is able to reply a image to a private message from valid users.

---
+++ chapter6/bot.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const mastodon = require('mastodon-api');
+const fs = require('fs');
+
+const M = new mastodon({
+  client_key: process.env.CLIENT_KEY,
+  client_secret: process.env.CLIENT_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  timeout_ms: 60 * 1000,
+  api_url: 'https://mastodon.social/api/v1/'
+});
+
+const listener = M.stream('streaming/direct');
+
+listener.on('message', evt => {
+  // load config file and check if user is allowed to get data from bot
+  let config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+  let ids = config.map(elt => {
+    return {
+      id: elt.user_id,
+      acct: elt.user_account
+    };
+  });
+  let userID = evt.data.last_status.account.id;
+  let userName = evt.data.last_status.account.username;
+  let account = evt.data.last_status.account.acct;
+  let valid = ids.reduce((current, elt) =>
+    (elt.id == userID && elt.acct == account), false);
+
+  if (valid) {
+
+    // generating Image
+
+
+
+    replyMedia(account, evt.data.last_status.id)
+      .then(response => console.log(response))
+      .catch(error => console.error(error));
+
+  }
+  //  fs.writeFileSync('./data.json', JSON.stringify(evt));
+  //console.log(evt)
+});
+
+
+listener.on('error', err => console.log(err));
+
+console.log('Bot started');
+
+
+
+async function replyMedia(acct, reply_id) {
+  //upload replyMedia
+  const stream = fs.createReadStream('./out.png');
+  const mediaParams = {
+    file: stream,
+    description: 'A wonderful chart.'
+  };
+  const response = await M.post('media', mediaParams);
+  const mediaID = response.data.id;
+
+  // send reply
+  const replyParams = {
+    status: `@${acct} a chart!`,
+    in_reply_to_id: reply_id,
+    media_ids: [mediaID],
+    visibility: 'direct'
+  }
+  const replyReponse = await M.post('statuses', replyParams);
+
+  return {
+    success: true
+  };
+}
